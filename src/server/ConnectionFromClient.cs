23364ec7ec56e22e33cb45117d4b004da5eac663
sergio.l.para@gmail.com <>
2021-12-18 16:50:03 +0000

    Implement the server counterpart of ConnectionToServer - the ConnectionFromClient.

--- src/server/ConnectionFromClient.cs
+++ src/server/ConnectionFromClient.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -27,8 +28,45 @@ internal class ConnectionFromClient
 
     void ProcessMethodCall()
     {
+        mReader ??= new(mRpcSocket.Stream);
+        mWriter ??= new(mRpcSocket.Stream);
 
+        byte[] protocolCapabilities = new byte[8];
+
+        byte protocolVersion = mReader.ReadByte();
+        mReader.Read(protocolCapabilities, 0, 8);
+
+        mWriter.Write(protocolCapabilities); // For now we return the same capabilities we read
+        mWriter.Flush();
+
+        byte method = mReader.ReadByte();
+
+        // All of this must be tidied up...
+        switch (method)
+        {
+            case ((byte)255):
+                ProcessEchoRequest(mReader, mWriter);
+                break;
+
+
+            default:
+                // What happens if we invoked a non-supported method...?
+                break;
+        }
+
+        mWriter.Flush();
     }
+
+    static void ProcessEchoRequest(BinaryReader reader, BinaryWriter writer)
+    {
+        string echoRequest = reader.ReadString();
+        string reply = $"Reply: {echoRequest}";
+
+        writer.Write(reply);
+    }
+
+    BinaryReader? mReader;
+    BinaryWriter? mWriter;
 
     readonly RpcSocket mRpcSocket;
 }
