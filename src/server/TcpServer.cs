a6a8bfe1ff5d586af14f918322d65d437825a50d
sergio.l.para@gmail.com <>
2021-12-18 15:57:06 +0000

    Add a first draft of the TcpServer class. It will be the main entry point for server-acting applications: it starts a TcpListener and waits for connections... for now!

---
+++ src/server/TcpServer.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace dotnetRpc.Server;
+
+public interface IServer
+{
+    Task ListenAsync(CancellationToken ct);
+}
+
+public class TcpServer : IServer
+{
+    public TcpServer(IPEndPoint bindTo)
+    {
+        mBindEndpoint = bindTo;
+    }
+
+    Task IServer.ListenAsync(CancellationToken ct)
+        => Task.Factory.StartNew(AcceptLoop, ct, TaskCreationOptions.LongRunning);
+
+    async Task AcceptLoop(object? state)
+    {
+        CancellationToken ct = (CancellationToken)state!;
+        TcpListener tcpListener = new(mBindEndpoint);
+
+        while (!ct.IsCancellationRequested)
+        {
+            try
+            {
+                Socket socket = await tcpListener.AcceptSocketAsync(ct);
+
+                // TODO: Maybe this socket needs some settings, define and apply them
+                // TODO: Handle the call (easy...!)
+            }
+            catch (SocketException ex)
+            {
+                // TODO: Handle the exception
+                throw;
+            }
+            catch (Exception ex)
+            {
+                // TODO: Handle the exception
+                throw;
+            }
+        }
+    }
+
+    readonly IPEndPoint mBindEndpoint;
+}
