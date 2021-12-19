6c6a4bebbc387bddd8f4b206898a5090c6174ab2
sergio.l.para@gmail.com <>
2021-12-19 16:44:03 +0000

    The RpcSocket is now created with a CancellationToken. When the token is cancelled, the socket is closed.

--- src/server/RpcSocket.cs
+++ src/server/RpcSocket.cs
@@ -13,16 +13,28 @@ internal class RpcSocket
     internal MeteredStream Stream => mMeteredStream;
     internal IPEndPoint RemoteEndPoint => mRemoteEndPoint;
 
-    internal RpcSocket(Socket socket)
+    internal RpcSocket(Socket socket, CancellationToken ct)
     {
         mSocket = socket;
         mMeteredStream = new(new NetworkStream(mSocket));
         mRemoteEndPoint = (IPEndPoint)mSocket.RemoteEndPoint!;
+
+        ct.Register(CloseSocket, socket);
     }
 
     internal async ValueTask BeginReceiveAsync(CancellationToken ct)
     {
         await mSocket.ReceiveAsync(Memory<byte>.Empty, SocketFlags.None, ct);
+    }
+
+    static void CloseSocket(object? state)
+    {
+        Socket? socket = state as Socket;
+        if (socket == null)
+            return;
+
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
     }
 
     readonly Socket mSocket;
