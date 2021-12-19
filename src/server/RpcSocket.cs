5624ad06b0c4bab6808d6d1a3512993b3978f181
sergio.l.para@gmail.com <>
2021-12-19 16:31:00 +0000

    Keep track of the remote IP endpoint in RpcSocket.

--- src/server/RpcSocket.cs
+++ src/server/RpcSocket.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,11 +11,13 @@ namespace dotnetRpc.Server;
 internal class RpcSocket
 {
     internal MeteredStream Stream => mMeteredStream;
+    internal IPEndPoint RemoteEndPoint => mRemoteEndPoint;
 
     internal RpcSocket(Socket socket)
     {
         mSocket = socket;
         mMeteredStream = new(new NetworkStream(mSocket));
+        mRemoteEndPoint = (IPEndPoint)mSocket.RemoteEndPoint!;
     }
 
     internal async ValueTask BeginReceiveAsync(CancellationToken ct)
@@ -24,4 +27,5 @@ internal class RpcSocket
 
     readonly Socket mSocket;
     readonly MeteredStream mMeteredStream;
+    readonly IPEndPoint mRemoteEndPoint;
 }
