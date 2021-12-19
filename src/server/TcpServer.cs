49c7788add32ae7d97d38b1cf9e9a2d88ab99ddd
sergio.l.para@gmail.com <>
2021-12-19 16:33:26 +0000

    Create the metrics and expose its counters on TcpServer. Also, pass the metrics down so the ConnectionFromClient can use them.

--- src/server/TcpServer.cs
+++ src/server/TcpServer.cs
@@ -3,6 +3,9 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+using dotnetRpc.Shared;
 
 namespace dotnetRpc.Server;
 
@@ -13,9 +16,13 @@ public interface IServer
 
 public class TcpServer : IServer
 {
+    public RpcMetrics.RpcCounters MetricCounters => mMetrics.Counters;
+
     public TcpServer(IPEndPoint bindTo)
     {
+        mMetrics = new();
         mBindEndpoint = bindTo;
+        mLog = RpcLoggerFactory.CreateLogger("TcpServer");
     }
 
     Task IServer.ListenAsync(CancellationToken ct)
@@ -33,7 +40,9 @@ public class TcpServer : IServer
             {
                 Socket socket = await tcpListener.AcceptSocketAsync(ct);
                 RpcSocket rpcSocket = new(socket);
-                ConnectionFromClient connectionFromClient = new(rpcSocket);
+                ConnectionFromClient connectionFromClient = new(mMetrics, rpcSocket);
+
+                mLog.LogTrace("New connection stablished from {0}", rpcSocket.RemoteEndPoint);
 
                 // TODO: Maybe this socket needs some settings, define and apply them
 
@@ -53,5 +62,7 @@ public class TcpServer : IServer
         }
     }
 
+    readonly RpcMetrics mMetrics;
     readonly IPEndPoint mBindEndpoint;
+    readonly ILogger mLog;
 }
