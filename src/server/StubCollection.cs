4e6054a5a98c528140ac8ff35b569bc058b230cc
sergio.l.para@gmail.com <>
2022-02-25 15:53:04 +0000

    Add the StubCollection class. It will hold all of the IStub implementations on the server. Bear in mind that implementations of IMethodId will need to override both GetHashCode and Equals... so maybe IMethodId should also implement the (IEqualatable?) interface. Let's see.

---
+++ src/server/StubCollection.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using dotnetRpc.Shared;
+
+namespace dotnetRpc.Server;
+
+public class StubCollection
+{
+    internal IStub? FindStub(IMethodId methodId)
+    {
+        return mStubList.FirstOrDefault(s => s.CanHandleMethod(methodId));
+    }
+
+    public void RegisterStub(IStub stub)
+    {
+        foreach (IMethodId methodId in stub.GetHandledMethods())
+        {
+            if (mMethodNames.ContainsKey(methodId))
+            {
+                Environment.FailFast(
+                    "The StubCollection already has a stub that handles"
+                    + $" MethodId {methodId}. Execution is going to be"
+                    + " halted immediately to prevent possible errors"
+                    + " (including data loss), as there is an initialization"
+                    + " error at the very core.");
+            }
+
+            mMethodNames.Add(methodId, methodId.Name);
+        }
+
+        mStubList.Add(stub);
+    }
+
+    internal string? SolveMethodName(IMethodId methodId)
+    {
+        if (mMethodNames.TryGetValue(methodId, out string? result))
+            return result;
+
+        return string.Empty;
+    }
+
+    readonly List<IStub> mStubList = new();
+    readonly Dictionary<IMethodId, string?> mMethodNames = new();
+}
