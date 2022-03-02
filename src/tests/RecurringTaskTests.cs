75a608f80f6cd750c11970d05092add66265a951
sergio.l.para@gmail.com <>
2022-03-02 21:59:46 +0000

    Cover RecurringTask with new tests.

--- src/tests/RecurringTaskTests.cs
+++ src/tests/RecurringTaskTests.cs
@@ -14,10 +14,10 @@ public class RecurringTaskTests
     [Test, Timeout(TestsConstants.Timeout)]
     public async Task Task_Starts_And_Stops()
     {
-        TestRecurringTask testRecurringTask = new();
+        RecurringTaskCountInvocations testRecurringTask = new();
         RecurringTask recurringTask = new(
             testRecurringTask.InvokeAsync,
-            nameof(TestRecurringTask));
+            nameof(RecurringTaskCountInvocations));
 
         const int minimumInvokations = 5;
         const int timerIntervalMs = 20;
@@ -46,10 +46,10 @@ public class RecurringTaskTests
     {
         CancellationTokenSource cts = new();
 
-        TestRecurringTask testRecurringTask = new();
+        RecurringTaskCountInvocations testRecurringTask = new();
         RecurringTask recurringTask = new(
             testRecurringTask.InvokeAsync,
-            nameof(TestRecurringTask));
+            nameof(RecurringTaskCountInvocations));
 
         const int minimumInvokations = 5;
         const int timerIntervalMs = 20;
@@ -72,9 +72,162 @@ public class RecurringTaskTests
 
         Assert.That(testRecurringTask.TimesInvoked, Is.EqualTo(actualTimesInvoked));
     }
+
+    [Test, Timeout(TestsConstants.Timeout)]
+    public async Task FireEarly_Works_And_Does_Not_Increment_TimesInvoked()
+    {
+        RecurringTaskCountInvocations testRecurringTask = new();
+        RecurringTask recurringTask = new(
+            testRecurringTask.InvokeAsync,
+            nameof(RecurringTaskCountInvocations));
+
+        const int minimumInvokations = 5;
+        const int timerIntervalMs = 20;
+
+        recurringTask.Start(
+            TimeSpan.FromMilliseconds(timerIntervalMs),
+            CancellationToken.None);
+
+        Assert.That(() => recurringTask.IsRunning, Is.True.After(100, 10));
+        Assert.That(
+            () => testRecurringTask.TimesInvoked,
+            Is.GreaterThanOrEqualTo(minimumInvokations)
+                .After(timerIntervalMs * (minimumInvokations + 1)));
+
+        await recurringTask.FireEarly();
+
+        await recurringTask.Stop();
+        
+        Assert.That(() => recurringTask.IsRunning, Is.False.After(100, 10));
+
+        Assert.That(
+            recurringTask.TimesInvoked,
+            Is.EqualTo(testRecurringTask.TimesInvoked - 1));
+    }
+
+    [Test, Timeout(TestsConstants.Timeout)]
+    public async Task FireEarly_Throws_Exception_From_Recurring_Action()
+    {
+        CancellationTokenSource cts = new();
+
+        RecurringTaskThrowsException testRecurringTask = new();
+        RecurringTask recurringTask = new(
+            testRecurringTask.InvokeAsync,
+            nameof(RecurringTaskCountInvocations));
+
+        const int minimumInvokations = 5;
+        const int timerIntervalMs = 20;
+
+        Assert.That(recurringTask.LastException, Is.Null);
+        
+        recurringTask.Start(TimeSpan.FromMilliseconds(timerIntervalMs), cts.Token);
+
+        Assert.That(() => recurringTask.IsRunning, Is.True.After(100, 10));
+
+        Assert.That(
+            () => recurringTask.TimesInvoked,
+            Is.GreaterThanOrEqualTo(minimumInvokations)
+                .After(timerIntervalMs * (minimumInvokations + 1)));
+
+        Assert.That(
+            recurringTask.LastException,
+            Is.Not.Null.And.TypeOf<InvalidOperationException>());
+
+        Assert.That(
+            () => recurringTask.FireEarly().GetAwaiter().GetResult(),
+            Throws.InvalidOperationException);
+
+        await recurringTask.Stop();
+
+        Assert.That(() => recurringTask.IsRunning, Is.False.After(100, 10));
+
+        uint actualTimesInvoked = recurringTask.TimesInvoked;
+
+        await Task.Delay(100);
+
+        Assert.That(recurringTask.TimesInvoked, Is.EqualTo(actualTimesInvoked));
+    }
+
+    [Test, Timeout(TestsConstants.Timeout)]
+    public async Task Stopping_An_Errored_Loop_Does_Not_Throw_Exception()
+    {
+        CancellationTokenSource cts = new();
+
+        RecurringTaskThrowsException testRecurringTask = new();
+        RecurringTask recurringTask = new(
+            testRecurringTask.InvokeAsync,
+            nameof(RecurringTaskCountInvocations));
+
+        const int minimumInvokations = 5;
+        const int timerIntervalMs = 20;
+
+        Assert.That(recurringTask.LastException, Is.Null);
+        
+        recurringTask.Start(TimeSpan.FromMilliseconds(timerIntervalMs), cts.Token);
+
+        Assert.That(() => recurringTask.IsRunning, Is.True.After(100, 10));
+
+        Assert.That(
+            () => recurringTask.TimesInvoked,
+            Is.GreaterThanOrEqualTo(minimumInvokations)
+                .After(timerIntervalMs * (minimumInvokations + 1)));
+
+        Assert.That(
+            recurringTask.LastException,
+            Is.Not.Null.And.TypeOf<InvalidOperationException>());
+
+        await recurringTask.Stop();
+
+        Assert.That(() => recurringTask.IsRunning, Is.False.After(100, 10));
+
+        uint actualTimesInvoked = recurringTask.TimesInvoked;
+
+        await Task.Delay(100);
+
+        Assert.That(recurringTask.TimesInvoked, Is.EqualTo(actualTimesInvoked));
+    }
+
+    [Test, Timeout(TestsConstants.Timeout)]
+    public async Task Task_Keeps_Running_Even_If_Errored()
+    {
+        CancellationTokenSource cts = new();
+
+        RecurringTaskThrowsException testRecurringTask = new();
+        RecurringTask recurringTask = new(
+            testRecurringTask.InvokeAsync,
+            nameof(RecurringTaskCountInvocations));
+
+        const int minimumInvokations = 5;
+        const int timerIntervalMs = 20;
+
+        Assert.That(recurringTask.LastException, Is.Null);
+        
+        recurringTask.Start(TimeSpan.FromMilliseconds(timerIntervalMs), cts.Token);
+
+        Assert.That(() => recurringTask.IsRunning, Is.True.After(100, 10));
+
+        Assert.That(
+            () => recurringTask.TimesInvoked,
+            Is.GreaterThanOrEqualTo(minimumInvokations)
+                .After(timerIntervalMs * (minimumInvokations + 1)));
+
+        Assert.That(
+            recurringTask.LastException,
+            Is.Not.Null.And.TypeOf<InvalidOperationException>());
+
+        cts.Cancel();
+
+        Assert.That(() => recurringTask.IsRunning, Is.False.After(100, 10));
+
+        uint actualTimesInvoked = recurringTask.TimesInvoked;
+
+        await Task.Delay(100);
+
+        Assert.That(recurringTask.TimesInvoked, Is.EqualTo(actualTimesInvoked));
+    }
 }
 
-class TestRecurringTask
+class RecurringTaskCountInvocations
 {
     public int TimesInvoked = 0;
 
@@ -82,5 +235,15 @@ class TestRecurringTask
     {
         await Task.Yield();
         Interlocked.Increment(ref TimesInvoked);
+    }
+}
+
+class RecurringTaskThrowsException
+{
+    public async Task InvokeAsync(CancellationToken ct)
+    {
+        await Task.Yield();
+        throw new InvalidOperationException(
+            "The InvokeAsync operation is not supported");
     }
 }
