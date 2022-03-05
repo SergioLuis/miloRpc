ef1cba9cd8328c862243d2b58a0ae956e6fe95ed
sergio.l.para@gmail.com <>
2022-03-05 16:25:45 +0000

    Rename the class name to match its file name.

--- src/tests/TestingConstants.cs
+++ src/tests/TestingConstants.cs
@@ -1,7 +1,8 @@
 namespace dotnetRpc.Tests;
 
-public static class TestsConstants
+public static class TestingConstants
 {
     public const int Timeout = 10_000;
+    public const int Retries = 3;
     public const int Port = 9876;
 }
