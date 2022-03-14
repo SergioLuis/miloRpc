4e62000b62b23c967ee19125fceda4a5a5c9d567
sergio.l.para@gmail.com <>
2022-03-14 19:38:51 +0000

    Add extension methods to read from a stream until the count is fullfilled, or until we don't have any more bytes to read from the underlying stream.

---
+++ src/dotnetRpc.Core/shared/StreamExtensionMethods.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+
+namespace dotnetRpc.Core.Shared;
+
+public static class StreamExtensionMethods
+{
+    public static void ReadUntilCountFulfilled(
+        this Stream st, Span<byte> destination)
+    {
+        int read = 0;
+        while (read < destination.Length)
+        {
+            int nextBlock = st.Read(destination.Slice(read));
+            if (nextBlock == 0)
+                throw new IOException("No more bytes to consume");
+            read += nextBlock;
+        }
+    }
+
+    public static void ReadUntilCountFulfilled(
+        this Stream st, byte[] buffer, int offset, int count)
+    {
+        int read = 0;
+        while (read < count)
+        {
+            int nextBlock = st.Read(buffer, offset + read, count - read);
+            if (nextBlock == 0)
+                throw new IOException("No more bytes to consume");
+            read += nextBlock;
+        }
+    }
+}
