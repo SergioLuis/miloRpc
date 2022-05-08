1f94643f20a4b5a5949e6c3d1398df642b2efe13
sergio.l.para@gmail.com <>
2022-05-08 11:18:40 +0000

    Add constructors to the generic INetworkMessage implementations. Next version now should be at least 0.1.1-beta.

--- src/miloRPC.Serialization/NetworkMessages.cs
+++ src/miloRPC.Serialization/NetworkMessages.cs
@@ -13,6 +13,13 @@ public class NetworkMessage<T> : INetworkMessage
 {
     public T? Val1 { get; set; }
 
+    public NetworkMessage() { }
+
+    public NetworkMessage(T? val1)
+    {
+        Val1 = val1;
+    }
+
     public void Deserialize(BinaryReader reader)
     {
         Val1 = Serializer<T>.Deserialize(reader);
@@ -28,6 +35,14 @@ public class NetworkMessage<T1, T2> : INetworkMessage
 {
     public T1? Val1 { get; set; }
     public T2? Val2 { get; set; }
+
+    public NetworkMessage() { }
+
+    public NetworkMessage(T1? val1, T2? val2)
+    {
+        Val1 = val1;
+        Val2 = val2;
+    }
 
     public void Deserialize(BinaryReader reader)
     {
@@ -47,6 +62,15 @@ public class NetworkMessage<T1, T2, T3> : INetworkMessage
     public T1? Val1 { get; set; }
     public T2? Val2 { get; set; }
     public T3? Val3 { get; set; }
+
+    public NetworkMessage() { }
+
+    public NetworkMessage(T1? val1, T2? val2, T3? val3)
+    {
+        Val1 = val1;
+        Val2 = val2;
+        Val3 = val3;
+    }
 
     public void Deserialize(BinaryReader reader)
     {
@@ -69,6 +93,16 @@ public class NetworkMessage<T1, T2, T3, T4> : INetworkMessage
     public T2? Val2 { get; set; }
     public T3? Val3 { get; set; }
     public T4? Val4 { get; set; }
+
+    public NetworkMessage() { }
+
+    public NetworkMessage(T1? val1, T2? val2, T3? val3, T4? val4)
+    {
+        Val1 = val1;
+        Val2 = val2;
+        Val3 = val3;
+        Val4 = val4;
+    }
 
     public void Deserialize(BinaryReader reader)
     {
@@ -94,6 +128,17 @@ public class NetworkMessage<T1, T2, T3, T4, T5> : INetworkMessage
     public T3? Val3 { get; set; }
     public T4? Val4 { get; set; }
     public T5? Val5 { get; set; }
+
+    public NetworkMessage() { }
+
+    public NetworkMessage(T1? val1, T2? val2, T3? val3, T4? val4, T5? val5)
+    {
+        Val1 = val1;
+        Val2 = val2;
+        Val3 = val3;
+        Val4 = val4;
+        Val5 = val5;
+    }
 
     public void Deserialize(BinaryReader reader)
     {
@@ -122,6 +167,18 @@ public class NetworkMessage<T1, T2, T3, T4, T5, T6> : INetworkMessage
     public T4? Val4 { get; set; }
     public T5? Val5 { get; set; }
     public T6? Val6 { get; set; }
+
+    public NetworkMessage() { }
+
+    public NetworkMessage(T1? val1, T2? val2, T3? val3, T4? val4, T5? val5, T6? val6)
+    {
+        Val1 = val1;
+        Val2 = val2;
+        Val3 = val3;
+        Val4 = val4;
+        Val5 = val5;
+        Val6 = val6;
+    }
 
     public void Deserialize(BinaryReader reader)
     {
@@ -153,6 +210,20 @@ public class NetworkMessage<T1, T2, T3, T4, T5, T6, T7> : INetworkMessage
     public T5? Val5 { get; set; }
     public T6? Val6 { get; set; }
     public T7? Val7 { get; set; }
+
+    public NetworkMessage() { }
+
+    public NetworkMessage(
+        T1? val1, T2? val2, T3? val3, T4? val4, T5? val5, T6? val6, T7? val7)
+    {
+        Val1 = val1;
+        Val2 = val2;
+        Val3 = val3;
+        Val4 = val4;
+        Val5 = val5;
+        Val6 = val6;
+        Val7 = val7;
+    }
 
     public void Deserialize(BinaryReader reader)
     {
@@ -187,6 +258,21 @@ public class NetworkMessage<T1, T2, T3, T4, T5, T6, T7, T8> : INetworkMessage
     public T6? Val6 { get; set; }
     public T7? Val7 { get; set; }
     public T8? Val8 { get; set; }
+
+    public NetworkMessage() { }
+
+    public NetworkMessage(
+        T1? val1, T2? val2, T3? val3, T4? val4, T5? val5, T6? val6, T7? val7, T8? val8)
+    {
+        Val1 = val1;
+        Val2 = val2;
+        Val3 = val3;
+        Val4 = val4;
+        Val5 = val5;
+        Val6 = val6;
+        Val7 = val7;
+        Val8 = val8;
+    }
 
     public void Deserialize(BinaryReader reader)
     {
