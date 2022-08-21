86b6ea5b27718e85f7dd079b893ad56e0ad8e96b
sergio.l.para@gmail.com <>
2022-08-21 15:24:58 +0000

    Add ConnectionSettings class to encapsulate settings related to Ssl and buffering. I think that now that I'm at it I'll use this class too to signal capability enablement (getting rid of the RpcCapabilities enum - or at least make it private) and add compression settings here too (including the ArrayPool that right now I'm passing as a parameter).

--- src/miloRPC.Core/shared/RpcProtocol.cs
+++ src/miloRPC.Core/shared/RpcProtocol.cs
@@ -1,5 +1,8 @@
+using System;
 using System.IO;
 using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace miloRPC.Core.Shared;
@@ -31,6 +34,39 @@ public interface IMethodId
     string Name { get; }
 
     void SetSolvedMethodName(string? name);
+}
+
+public class ConnectionSettings
+{
+    public SslSettings Ssl { get; init; } = SslSettings.None;
+    public BufferingSettings Buffering { get; init; } = BufferingSettings.None;
+
+    public static readonly ConnectionSettings None = new();
+
+    public class SslSettings
+    {
+        public string? CertificatePath { get; init; } = string.Empty;
+        public string? CertificatePassword { get; init; } = string.Empty;
+        public Func<object, X509Certificate?, X509Chain?, SslPolicyErrors, bool>? CertificateValidationCallback { get; init; } = null;
+
+        public static readonly SslSettings None = new();
+
+        public static readonly Func<object, X509Certificate?, X509Chain?, SslPolicyErrors, bool> AcceptAllCertificates = (_, _, _, _) => true;
+    }
+
+    public class BufferingSettings
+    {
+        public bool Enable { get; init; } = false;
+        public int BufferSize { get; init; } = -1;
+
+        public static readonly BufferingSettings None = new();
+
+        public static readonly BufferingSettings Recommended = new()
+        {
+            Enable = true,
+            BufferSize = 4096
+        };
+    }
 }
 
 public interface INegotiateRpcProtocol
