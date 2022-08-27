e472c2992db744d70483263e853f2f076206ef97
sergio.l.para@gmail.com <>
2022-08-27 16:46:18 +0000

    Remove warning, as it turned out not to be the case (as one could've hoped, I don't know what was I thinking...)

--- src/miloRPC.Core/shared/RpcBufferedStream.cs
+++ src/miloRPC.Core/shared/RpcBufferedStream.cs
@@ -44,7 +44,6 @@ public class RpcBufferedStream : Stream
 
     public override int Read(byte[] buffer, int offset, int count)
     {
-        #warning Reads with 0 count might return immediately when done against the BufferedStream.
         return mInputStream.Read(buffer, offset, count);
     }
 
