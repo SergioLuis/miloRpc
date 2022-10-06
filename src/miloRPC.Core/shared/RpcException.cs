fb6c893790c628f8e1e4caa6972c3f78cb204eaa
sergio.l.para@gmail.com <>
2022-10-06 21:04:58 +0000

    Add a new kind of exception This exception is to be thrown when disposing the stream that comes from the network (DestinationStreamMessage) without it being fully consumed.

--- src/miloRPC.Core/shared/RpcException.cs
+++ src/miloRPC.Core/shared/RpcException.cs
@@ -6,7 +6,16 @@ public class RpcException : Exception
 {
     public RpcException(string message)
         : base(message) { }
-    
+
     public RpcException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
+
+public class StreamNotConsumedRpcException : RpcException
+{
+    public StreamNotConsumedRpcException(string message)
+        : base(message) { }
+
+    public StreamNotConsumedRpcException(string message, Exception innerException)
         : base(message, innerException) { }
 }
